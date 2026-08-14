c962ed3c88340714df2d41b649cebf913252a400
Weeez1 <andrejlukas830@gmail.com>
2026-08-14 14:31:25 +0300

    Update main.py
    Connect main with AI module

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from bot.ai import ask_ai
+
 def main():
-    print("AI Telegram Bot")
+    message = "Привет, AI!"
+    response = ask_ai(message)
+    
+    print(response)
     
 if name == "__main__":
     main()
